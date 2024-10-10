7482370bd0765e9d5b2e9ba3152016ce1ac3a0a8
Amna <ammy5342@gmail.com>
2024-10-10 03:15:53 +0300

    Encapsulated starter code in Tab class

--- main.py
+++ main.py
@@ -1,64 +1,93 @@
 from tkinter import *
 from tkinter import ttk
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-def loadSignal(file):
-    return 0
-def displaySignal(file):
-    return 0
-def addSignals(signal_a,signal_b):
-    return 0
-def subtractSignals(signal_a,signal_b):
-    return 0
-def multiplySignals(signal_a,signal_b):
-    return 0
-def scaleSignal(signal):
-    return 0
-def divideSignals(signal_a,signal_b):
-    return 0
+class UI:
+    def initialize(self):
+        root = Tk()
+        root.title("Digital Signal Processing")
+        root.columnconfigure(0, weight=1)
+        root.rowconfigure(0, weight=1)
 
-root = Tk()
-root.title("Digital Signal Processing")
+        notebook = ttk.Notebook(root)
+        notebook.grid(column=0, row=0, sticky=(N, W, E, S))  # Use grid for notebook
+        return root, notebook
 
-notebook = ttk.Notebook(root)
-notebook.grid(column=0, row=0, sticky=(N, W, E, S))  # Use grid for notebook
+    def loadSignal(file):
+        return 0
 
+    def displaySignal(file):
+        return 0
+
+class Tab:
 #Task 1
+    def __init__(self,notebook,name):
+        self.notebook=notebook
+        self.name=name
+        self.frame = ttk.Frame(notebook,padding="3 3 12 12")
+        sig_A= ttk.Button(self.frame,text="Signal A")
+        sig_B= ttk.Button(self.frame,text="Signal B")
+        fig = plt.Figure(figsize=(4, 3), dpi=100)
+        ax = fig.add_subplot(111)
+        canvas =  FigureCanvasTkAgg(fig, master=self.frame)
+        canvas.draw()
+        display=ttk.Button(self.frame,text="Display Signal")
 
-frame_1 = ttk.Frame(notebook,padding="3 3 12 12")
-sig_A= ttk.Button(frame_1,text="Signal A")
-sig_B= ttk.Button(frame_1,text="Signal B")
-AplusB= ttk.Button(frame_1,text="A + B")
-AminusB= ttk.Button(frame_1,text="A - B")
-AtimesB= ttk.Button(frame_1,text="A × B")
-AmulC= ttk.Button(frame_1,text="c × A")
-c_label=ttk.Label(frame_1,text="Value of c:")
-c=StringVar()
-c_input=ttk.Entry(frame_1,textvariable=c)
+        self.frame.grid(column=0, row=0,sticky=(W, E))
+        sig_A.grid(column=0, row=0,columnspan=2,sticky=(W, E))
+        sig_B.grid(column=2, row=0,columnspan=2,sticky=(W, E))
+        canvas.get_tk_widget().grid(column=0, row=3,columnspan=4)
 
+        display.grid(column=0,row=4,columnspan=4, sticky=(N, W, E, S))
+       
+    def add(self):
+        for child in self.frame.winfo_children(): 
+            child.grid_configure(padx=5, pady=5)
+        notebook.add(self.frame, text=self.name)
 
-frame_1.grid(column=0, row=0,sticky=(W, E))
-sig_A.grid(column=0, row=0,columnspan=2,sticky=(W, E))
-sig_B.grid(column=2, row=0,columnspan=2,sticky=(W, E))
-AplusB.grid(column=0, row=1)
-AminusB.grid(column=1, row=1)
-AtimesB.grid(column=2, row=1)
-AmulC.grid(column=3, row=1)
+class Task1(Tab):
+    def __init__(self,notebook,name):
+        super().__init__(notebook,name)
+        AplusB= ttk.Button(self.frame,text="A + B")
+        AminusB= ttk.Button(self.frame,text="A - B")
+        AtimesB= ttk.Button(self.frame,text="A × B")
+        AmulC= ttk.Button(self.frame,text="c × A")
+        c_label=ttk.Label(self.frame,text="Value of c:")
+        c=StringVar()
+        c_input=ttk.Entry(self.frame,textvariable=c)
 
-c_label.grid(column=0,row=2)
+        AplusB.grid(column=0, row=5)
+        AminusB.grid(column=1, row=5)
+        AtimesB.grid(column=2, row=5)
+        AmulC.grid(column=3, row=5)
+        c_label.grid(column=0,row=6)
+        c_input.grid(column=1,row=6,columnspan=3,sticky=(W, E))
 
-c_input.grid(column=1,row=2,columnspan=3,sticky=(W, E))
+    def addSignals(signal_a,signal_b):
+        return 0
 
-for child in frame_1.winfo_children(): 
-    child.grid_configure(padx=5, pady=5)
+    def subtractSignals(signal_a,signal_b):
+        return 0
 
-notebook.add(frame_1, text='Task 1')
+    def multiplySignals(signal_a,signal_b):
+        return 0
 
+    def scaleSignal(signal):
+        return 0
+        
+    def divideSignals(signal_a,signal_b):
+        return 0
 
+class Task2(Tab):
+    def init(self,notebook,name):
+        super().__init__(notebook,name)  # second page
 
-f2 = ttk.Frame(notebook)   # second page
-
-notebook.add(f2, text='Task 2')
-
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
+ui=UI()
+root,notebook = ui.initialize()
+t1=Task1(notebook, "Task 1")
+t1.add()
+t2= Task2(notebook, 'Task-2')
+t2.add()
 root.mainloop()
+
