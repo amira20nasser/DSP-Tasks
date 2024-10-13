5f0ecc144546fe6d44cf656a1696cc4962591e7c
Amna <ammy5342@gmail.com>
2024-10-13 23:31:27 +0300

    Defined figure attributes in tab object

--- ui/ui_widgets.py
+++ ui/ui_widgets.py
@@ -26,18 +26,20 @@ class Tab:
             self.frame = ttk.Frame(notebook,padding="3 3 12 12")
             sig_A= ttk.Button(self.frame,text="Signal A", command=lambda: self.loadSignal('A'))
             sig_B= ttk.Button(self.frame,text="Signal B",command=lambda: self.loadSignal('B'))
-            display=ttk.Button(self.frame,text="Display Signal",command=self.displaySignal)
+            save_output=ttk.Button(self.frame,text="Save Output Signal",command=self.saveOutput)
 
             self.frame.grid(column=0, row=0,sticky=(W, E))
-            sig_A.grid(column=0, row=0,columnspan=2,sticky=(W, E))
-            sig_B.grid(column=2, row=0,columnspan=2,sticky=(W, E))
-            self.ax_in, self.canvas_in = self.initialize_graph()
+            sig_A.grid(column=0, row=1,columnspan=2,sticky=(W, E))
+            sig_B.grid(column=2, row=1,columnspan=2,sticky=(W, E))
+            self.ax_in, self.canvas_in = self.initialize_graph('Input Signals','t','x(t)')
+            self.ax_out, self.canvas_out = self.initialize_graph('Output Signals','t','x(t)')
 
-            self.canvas_in.get_tk_widget().grid(column=0, row=3,columnspan=4)
-
-            display.grid(column=0,row=4,columnspan=4, sticky=(N, W, E, S))
+            self.canvas_in.get_tk_widget().grid(column=0, row=0,columnspan=4)
+            self.canvas_out.get_tk_widget().grid(column=4, row=0,columnspan=4)
+            save_output.grid(column=4,row=1,columnspan=4, sticky=(N, W, E, S))
         
         def add(self):
+            
             for child in self.frame.winfo_children(): 
                 child.grid_configure(padx=5, pady=5)
             self.notebook.add(self.frame, text=self.name)
@@ -57,16 +59,16 @@ class Tab:
                 self.plot_graph(self.ax_in,self.canvas_in,self.Bx,self.By);       
      
         
-        def displaySignal(self, file):
+        def saveOutput(self, file):
             print("display_signal Logic")
                 #return signal 
 
-        def initialize_graph(self):
+        def initialize_graph(self,title,xlabel,ylabel):
         
-            fig, ax = plt.subplots(figsize=(5, 5))
-            # ax.set_title('Signal')
-            # ax.set_xlabel('Indices')
-            # ax.set_ylabel('Samples')
+            fig, ax = plt.subplots(figsize=(5, 4))
+            ax.set_title(title)
+            ax.set_xlabel(xlabel)
+            ax.set_ylabel(ylabel)
             canvas = FigureCanvasTkAgg(fig, master=self.frame)
             # canvas_widget.pack( fill='both', expand=True)
             return ax,canvas
