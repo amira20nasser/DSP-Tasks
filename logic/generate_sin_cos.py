1adea03e8a05adb6f9cd4a32792fe94ddd9e5db8
amira_nasser <amira20nasser@gmail.com>
2024-10-20 19:49:46 +0300

    TASK2:  generate and plot sampling for sin or cos

--- logic/generate_sin_cos.py
+++ logic/generate_sin_cos.py
@@ -8,7 +8,7 @@ class Type(Enum):
 
 class GenerateSinCos:
     @staticmethod
-    def generate(sig_type : Type,amp,freq,shifted):
+    def generate_sin_cos(sig_type : Type,amp,freq,shifted):
         # t = np.linspace(0, 2 * np.pi, 1) 
         t = np.arange(0, 2 , 0.01)
         if sig_type == Type.SIN:
@@ -20,3 +20,16 @@ class GenerateSinCos:
             t=0
             x_t = -1
         return t,x_t    
+
+    @staticmethod
+    def sampling_sin_cos(sig_type : Type,amp,freq,shifted,fs):
+        n = np.arange(0, 2 ,(freq / fs)) 
+        if sig_type == Type.SIN:
+            x_n = amp * np.round(np.sin(2* np.pi * (freq / fs) *n +shifted),3)
+        elif sig_type == Type.COS:
+            x_n = amp * np.round(np.cos(2* np.pi * (freq / fs) *n +shifted),3)
+        else:
+            print("!!SAMPLING TYPE SIGNAL NOT SIN OR COS!!")
+            n=0
+            x_n = -1
+        return n,x_n        
--- ui/task2_ui.py
+++ ui/task2_ui.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 from ui.ui_widgets import Tab  
 from logic.generate_sin_cos import *
 from logic.basic_signal_operations import Signal
+from utils import *
 
 class Task2UI(Tab):
     def __init__(self, notebook, name):
@@ -13,8 +14,8 @@ class Task2UI(Tab):
         self.continous_out = None
         self.discrete_out = None
         
-        self.ax_in.plot(np.random.rand(3,),np.zeros(3))
-        self.canvas_in.draw()
+        # self.ax_in.plot(np.random.rand(1000000,),np.zeros(1000000))
+        # self.canvas_in.draw()
 
         label = ttk.Label(self.frame,text="Please select type:")
         label.grid(column=0, row=3,columnspan=2,sticky=(W, E))
@@ -44,13 +45,30 @@ class Task2UI(Tab):
         shifted_input.grid(column=2, row=6, columnspan=1,sticky=(N, W, E, S))
 
         generate_btn = ttk.Button(self.frame, text="Generate SIN / COS",command=self.on_click_generate)
-        generate_btn.grid(column=1, row=7,sticky=(W, E))
+        generate_btn.grid(column=0, row=7,columnspan=3,sticky=(W, E))
+
+        label = ttk.Label(self.frame,text="F_s")
+        label.grid(column=0, row=8,columnspan=1,sticky=(W, E))
+        self.fs= IntVar()
+        fs_input = ttk.Entry(self.frame, textvariable=self.fs)
+        fs_input.grid(column=1, row=8, columnspan=1,sticky=(N, W, E, S))
+        sampling_btn = ttk.Button(self.frame, text=f"Sampling {self.selected_type.get()} Wave",command=self.on_click_Sampling)
+        sampling_btn.grid(column=0, row=9,columnspan=3,sticky=(W, E))
 
     def on_click_generate(self):
-        t,x_t = GenerateSinCos.generate(Type[self.selected_type.get()],self.amplitude.get(),self.freq.get(),self.shifted.get())
+        t,x_t = GenerateSinCos.generate_sin_cos(Type[self.selected_type.get()],self.amplitude.get(),self.freq.get(),self.shifted.get())
         signal = Signal(t,x_t)
         continous_out = signal
         self.plot_continous_graph(self.ax_in,self.canvas_in,signal,f"{self.selected_type.get()} Signal")
+
+    def on_click_Sampling(self):
+        if self.fs.get() < 2*self.freq.get():
+            show_message_box("ERROR" , f"fs must be at least {2*self.freq.get()} ")
+        n,x_n = GenerateSinCos.sampling_sin_cos(Type[self.selected_type.get()],self.amplitude.get(),self.freq.get(),self.shifted.get(),self.fs.get())
+        signal = Signal(n,x_n)
+        continous_out = signal
+        self.plot_discrete_graph(self.ax_out,self.canvas_out,signal,f"Sampling {self.selected_type.get()} Signal")
+
 
     def plot_continous_graph(self,ax,canvas,signal,title):
         ax.set_title(title)
