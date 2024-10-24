8af378d9534496e7258acaad8f1ec41ffd799ec2
amira_nasser <amira20nasser@gmail.com>
2024-10-24 17:14:51 +0300

    initial Task3UI and integrate into main application

--- main.py
+++ main.py
@@ -2,11 +2,15 @@
 from ui.ui_widgets import UI
 from ui.task1_ui import Task1UI
 from ui.task2_ui import Task2UI
+from ui.task3_ui import Task3UI
 
 def main():
     ui = UI()
     root, notebook = ui.initialize()
-    
+
+    task3_tab = Task3UI(notebook, "Task 3")
+    task3_tab.add()
+
     task2_tab = Task2UI(notebook, "Task 2")
     task2_tab.add()
 
---
+++ ui/task3_ui.py
@@ -0,0 +1,5 @@
+from ui.ui_widgets import *
+class Task3UI(Tab):
+    def __init__(self,notebook,name):
+        super().__init__(notebook,name)
+
