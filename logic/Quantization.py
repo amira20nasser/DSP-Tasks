e1a905dbdcfb2101726c9e7d419170678aed9b48
Amna <ammy5342@gmail.com>
2024-10-27 19:53:36 +0300

    Attempting to fix bug in quantization

--- logic/Quantization.py
+++ logic/Quantization.py
@@ -15,13 +15,15 @@ class Quatization:
         print(f"#LEVELS->{levels}")
         step = np.round((mx_value - mn_value) / levels,4)
         print(f"Each step->{step}")
-        intervals = np.arange(mn_value, mx_value+step, step)
+        intervals = np.arange(mn_value, mx_value+step+step, step)
         print(f"intervals {intervals}")
-     
+        midpoints=(intervals[:-1]+intervals[1:])/2
+        print(f"midpoints {midpoints}")
         # return index of interval that belongs to 
-        interval_index = np.digitize(x_n, intervals)
+        
+        interval_index = np.digitize(np.clip(x_n,a_min=midpoints[0],a_max=midpoints[-1]), midpoints)
         print(f"index intervals{interval_index}")
-        x_q = [np.round((intervals[i - 1] + intervals[i]) / 2,4) for i in interval_index]
+        x_q = [np.round((midpoints[i - 1] + midpoints[i]) / 2,4) for i in interval_index]
 
         bits = int(np.log2(levels))
         encoded_index = [format(index-1,f'0{bits}b') for index in interval_index]
