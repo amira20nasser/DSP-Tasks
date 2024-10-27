62b6f5ef1e7a0571e466279cdf53123a716be40a
Amna <ammy5342@gmail.com>
2024-10-27 20:48:18 +0300

    Fixed quantization bug: handled bin edges

--- logic/Quantization.py
+++ logic/Quantization.py
@@ -15,15 +15,13 @@ class Quatization:
         print(f"#LEVELS->{levels}")
         step = np.round((mx_value - mn_value) / levels,4)
         print(f"Each step->{step}")
-        intervals = np.arange(mn_value, mx_value+step+step, step)
+        intervals = np.arange(mn_value, mx_value+step, step)
         print(f"intervals {intervals}")
-        midpoints=(intervals[:-1]+intervals[1:])/2
-        print(f"midpoints {midpoints}")
+        x_n = np.clip(x_n, a_min=intervals[0], a_max=intervals[-1])
         # return index of interval that belongs to 
-        
-        interval_index = np.digitize(np.clip(x_n,a_min=midpoints[0],a_max=midpoints[-1]), midpoints)
+        interval_index = np.digitize(x_n, intervals,right=True)
         print(f"index intervals{interval_index}")
-        x_q = [np.round((midpoints[i - 1] + midpoints[i]) / 2,4) for i in interval_index]
+        x_q = [np.round((intervals[i - 1] + intervals[i]) / 2,4) if i > 0 else intervals[0] for i in interval_index]
 
         bits = int(np.log2(levels))
         encoded_index = [format(index-1,f'0{bits}b') for index in interval_index]
