f6d4190434184fe8f5b29eaf7b206a79d4084690
Amna <ammy5342@gmail.com>
2024-12-04 00:47:24 +0200

    Incomplete UI

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from ui.task1_ui import Task1UI
 from ui.task2_ui import Task2UI
 from ui.task3_ui import Task3UI
 from ui.task4_ui import Task4UI
+from ui.task5_ui import Task5UI
+
 from logic.fourier_transform import *
 from logic.basic_signal_operations import*
 def main():
@@ -14,22 +16,25 @@ def main():
                                             [0,1.96349540849362,2.35619449019235,2.74889357189107 ,-3.14159265358979,-2.74889357189107,-2.35619449019235,-1.96349540849362]
     )
     """
-    x_k= FourierTransform.dtf_transform([1, 3, 5, 7, 9, 11, 13, 15],8)                                     
+    #x_k= FourierTransform.dtf_transform([1, 3, 5, 7, 9, 11, 13, 15],8)                                     
     # print(x_n)
-    # task4_tab = Task4UI(notebook,"Task 4")
-    # task4_tab.add()
+    task5_tab = Task5UI(notebook,"Task 5")
+    task5_tab.add()
 
-    # task3_tab = Task3UI(notebook, "Task 3")
-    # task3_tab.add()
+    task4_tab = Task4UI(notebook,"Task 4")
+    task4_tab.add()
 
-    # task2_tab = Task2UI(notebook, "Task 2")
-    # task2_tab.add()
+    task3_tab = Task3UI(notebook, "Task 3")
+    task3_tab.add()
 
-    # task1_tab = Task1UI(notebook, "Task 1")
-    # task1_tab.add()
+    task2_tab = Task2UI(notebook, "Task 2")
+    task2_tab.add()
+
+    task1_tab = Task1UI(notebook, "Task 1")
+    task1_tab.add()
 
 
-    # root.mainloop()
+    root.mainloop()
 
 main()
 
---
+++ ui/task5_ui .py
@@ -0,0 +1,114 @@
+import ttkbootstrap as ttk
+from ui.ui_widgets import *
+import os
+from logic.Quantization import *
+from task3_test.QuanTest1 import  *
+from task3_test.QuanTest2 import *
+from visualizer import *
+from file_manpulator import *
+
+class Task5UI(Tab):
+    def initialize_ui_variables(self):       
+        # self.save_file = StringVar()
+        self.error=None
+        self.time_domain_signal = None
+        self.frequency_domain_signal = None
+        self.fs = IntVar()
+        self.file_manpulator = FileManpulator()
+        self.time_amplitude_visualizer = Visualizer(self.frame,'Time Domain Signal','n','x(n)')
+        self.frequency_amplitude_visualizer = Visualizer(self.frame,'Frequency Domain (Amplitude)','k','x(k)')
+        self.frequency_phase_visualizer = Visualizer(self.frame,'Frequency Domain (Phase)','n','x_q(n)')
+
+    def __init__(self,notebook,name):
+        super().__init__(notebook,name)
+        self.initialize_ui_variables()
+
+        self.time_amplitude_visualizer.canvas.get_tk_widget().grid(column=0, row=0,)
+        self.frequency_amplitude_visualizer.canvas.get_tk_widget().grid(column=1, row=0,)
+        self.frequency_phase_visualizer.canvas.get_tk_widget().grid(column=1, row=2,)
+
+        upload_time_sig = ttk.Button(master=self.frame, text="Upload Sampled Signal",command=self.on_click_upload) 
+        upload_time_sig.grid(column=0,row=1,sticky=(W))
+
+        upload_freq_sig = ttk.Button(master=self.frame, text="Upload Sampled Signal",command=self.on_click_upload) 
+        upload_freq_sig.grid(column=0,row=3,sticky=(W))
+
+        self.isSelectedLevels.set(True)
+        radio_levels = ttk.Radiobutton(self.frame,text="#Levels", variable=self.isSelectedLevels,value=1,command=self.toggle_input)
+        self.levels_entry = ttk.Entry(self.frame,state=["normal"],textvariable=self.levels)
+
+        radio_levels.grid(column=0,row=2,sticky=(W),)
+        self.levels_entry.grid(column=0,row=3, sticky=(W))
+
+        radio_bits = ttk.Radiobutton(self.frame,text="#bits", variable=self.isSelectedLevels,value=0,command=self.toggle_input)
+        self.bits_entry = ttk.Entry(self.frame,state=["disabled"],textvariable=self.bits)
+        radio_bits.grid(column=0,row=4,sticky=(W))
+        self.bits_entry.grid(column=0,row=5, sticky=(W),)
+
+        quantized_sig = ttk.Button(master=self.frame, text="Show Quantized Signal",command=self.on_click_quantized) 
+        quantized_sig.grid(column=1,row=1,sticky=(W))
+
+        # save_output=ttk.Button(self.frame,text="Save Output",command=lambda: self.saveOutput(self.encoded_indices,self.Out,filename_entry.get()),bootstyle=(SUCCESS,OUTLINE))
+        clear_output=ttk.Button(self.frame,text="Clear Quantized Output",command=self.quantized_visualizer.clear_plotting,bootstyle=(SUCCESS,OUTLINE))
+        # save_output.grid(column=1,row=2,sticky=(W))
+        clear_output.grid(column=1,row=3,sticky=(W))
+        # filename_label = ttk.Label(self.frame, text="Save file name:")
+        # filename_entry = ttk.Entry(self.frame, textvariable=self.save_file)
+        # filename_label.grid(column=1,row=4,sticky=(W))
+        # filename_entry.grid(column=1,row=5,sticky=(W))
+        
+        err_sig = ttk.Button(master=self.frame, text="Show Error",command=self.on_click_show_err) 
+        err_sig.grid(column=2,row=1,sticky=(W))
+
+    def toggle_input(self):
+        if self.isSelectedLevels.get():
+            self.levels_entry.config(state=['normal'])
+            self.bits_entry.config(state=['disabled'])
+        else:
+            self.levels_entry.config(state=['disabled'])
+            self.bits_entry.config(state=['normal'])     
+    
+    
+    def on_click_upload(self):
+        x,y = self.file_manpulator.loadSignal()
+        self.sampled_signal = Signal(x,y)
+        self.original_visualizer.plot_discrete_graph(self.sampled_signal)
+    def on_click_quantized(self):
+        if self.sampled_signal == None or (self.levels_entry.get()==None and self.bits_entry==None):
+            messagebox.showerror("DSP", "MUST ENTER ALL VALUES sampled_sig,#levels,#bits")
+            return   
+
+        index_intervals,x_q,encoded_index = [],[] ,[]
+        if self.isSelectedLevels.get(): 
+            interval_index,x_q,encoded_index = Quatization.quantize_signal(self.sampled_signal,self.levels.get())
+        else:
+            interval_index,x_q,encoded_index = Quatization.quantize_signal(self.sampled_signal,2**self.bits.get())
+        
+        self.quantized_signal=Signal(self.sampled_signal.x , x_q)
+        self.quantized_visualizer.plot_discrete_graph(signal=self.quantized_signal)
+        QuantizationTest1("task3\Test 1\Quan1_Out.txt",encoded_index,x_q)
+        QuantizationTest2("task3\Test 2\Quan2_Out.txt",interval_index,encoded_index,x_q,Quatization.calculate_error(self.quantized_signal.y , self.sampled_signal.y))
+
+    def on_click_show_err(self):
+        error = Quatization.calculate_error(self.quantized_signal.y , self.sampled_signal.y)
+        self.error_visualizer.plot_continous_graph(Signal(self.sampled_signal.x,error))
+
+    # def saveOutput(self, interval_indices, quantized_values, file):
+    #     if interval_indices == None or quantized_values == None:
+    #         show_message_box("DSP" , "No output signal to save")
+    #     elif not file :
+    #         show_message_box("DSP" , "Please enter save file name")
+    #     else:
+    #         with open(file, 'w') as f:
+    #             # Write header information
+    #             f.write('0\n0\n' + str(len(interval_indices)) + '\n')
+                
+    #             # Iterate through both arrays and write to file
+    #             for interval_index, quantized_value in zip(interval_indices, quantized_values):
+    #                 f.write(f"{interval_index} {quantized_value}\n")
+
+    #         show_message_box("DSP" , "Signal saved successfully")
+
+
+
+    
